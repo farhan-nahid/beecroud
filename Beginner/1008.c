811992f27735dccf267739f10c73f1a06b2df2c4
farhan-nahid <nahidahmed9933@gmail.com>
2022-09-29 11:56:51 +0600

    1008 solved

---
+++ Beginner/1008.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+ 
+int main() {
+ int NUMBER, amount;
+ float hour, SALARY;
+
+
+ scanf("%d", &NUMBER);
+ scanf("%d", &amount);
+ scanf("%f", &hour);
+
+ SALARY = amount * hour;
+
+ printf("NUMBER = %d\n", NUMBER);
+ printf("SALARY = U$ %.2f\n", SALARY);
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+ 
+    return 0;
+}
