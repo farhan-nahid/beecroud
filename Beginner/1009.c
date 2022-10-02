b8aa504ce9b421a2d2fd5b58dbe1385a25f845e4
farhan-nahid <nahidahmed9933@gmail.com>
2022-10-02 18:46:32 +0600

    1009 solved

---
+++ Beginner/1009.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+    char name;
+    double a, b, total;
+
+    scanf("%s", &name);
+    scanf("%lf", &a);
+    scanf("%lf", &b);
+
+    total = a + b * .15 ;
+
+    printf("TOTAL = R$ %.2lf\n", total);
+
+    return 0;
+}
