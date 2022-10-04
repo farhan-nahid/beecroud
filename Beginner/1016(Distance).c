5522a026fe2e5178e98363e53db705e8b27d54f4
farhan-nahid <nahidahmed9933@gmail.com>
2022-10-04 13:39:37 +0600

    1016 solved

---
+++ Beginner/1016(Distance).c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+    int distance, result;
+
+    scanf("%d", &distance);
+    result = distance * 2;
+
+    printf("%d minutos\n", result);
+ 
+    return 0;
+}
