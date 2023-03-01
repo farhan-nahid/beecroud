5e41918c52fc70ee2c452d38b89da69ec9aba29a
Farhan Ahmed Nahid <74184886+farhan-nahid@users.noreply.github.com>
2023-03-01 23:50:40 +0600

    Update main.cpp

--- Beginner/cpp/1070(Six_Odd_Numbers)/main.cpp
+++ Beginner/cpp/1070(Six_Odd_Numbers)/main.cpp
@@ -9,7 +9,7 @@ int main(){
     cin >> input;
 
     while(odd_number_count < 6){
-        if(input %2 == 1){
+        if(input % 2 == 1){
             cout << input << endl;
             odd_number_count +=1;
         }
